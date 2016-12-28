8ce3284faf80242d91c89f005e831d97b127d0bf
Tyung <terry.wutongfei@gmail.com>
2016-12-28 15:36:18 +0800

    UGS配置修改

---
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSyncLauncher/NotForLicensees/ProgramSettings.cs
@@ -0,0 +1,14 @@
+	using System;
+	using System.Data.SqlClient;
+
+	namespace UnrealGameSyncLauncher
+{
+		static partial class Program
+		{
+			static Program()
+			{
+				StableSyncPath = "";
+				UnstableSyncPath = "";
+			}
+		}
+	}
