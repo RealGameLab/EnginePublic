de1d9d87b5ca4e757fce406db0fe69f7d4bf319a
Tyung <terry.wutongfei@gmail.com>
2017-04-26 18:41:35 +0800

    提交programsettings

---
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSyncLauncher/NotForLicensees/ProgramSettings.cs
@@ -0,0 +1,12 @@
+﻿
+namespace UnrealGameSyncLauncher
+{
+    static partial class Program
+    {
+        static Program()
+        {
+            StableSyncPath = "//Git/UEDev/Engine/Source/Programs/UnrealGameSync/UnrealGameSync/bin/UnstableRelease/...";
+            UnstableSyncPath = null;
+        }
+    }
+}
