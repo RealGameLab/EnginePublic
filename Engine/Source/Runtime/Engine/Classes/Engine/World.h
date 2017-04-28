0e4ab7e4bbabe8f28028d6f773163e914343e0ec
Tyung <terry.wutongfei@gmail.com>
2017-04-28 11:10:05 +0800

    ark更新

--- Engine/Source/Runtime/Engine/Classes/Engine/World.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/World.h
@@ -482,6 +482,7 @@ private:
 public:
 
 	bool IsRemoteOwned() const { return bRemoteOwned; }
+    void SetRemoteOwned(bool bInRemoteOwned) { bRemoteOwned = bInRemoteOwned; }
 
 	/* Determines whether spawning will not fail if certain conditions are not met. If true, spawning will not fail because the class being spawned is `bStatic=true` or because the class of the template Actor is not the same as the class of the Actor being spawned. */
 	uint16	bNoFail:1;
